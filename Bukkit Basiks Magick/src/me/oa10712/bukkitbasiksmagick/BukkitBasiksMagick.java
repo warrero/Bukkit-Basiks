ba4e68de1d1852b098d3e977c4c1063e2ba319ed
Dalton Myhr <warcoding@gmail.com>
2012-03-09 18:59:51 -0600

    added magick

--- Bukkit Basiks Magick/src/me/oa10712/bukkitbasiksmagick/BukkitBasiksMagick.java
+++ Bukkit Basiks Magick/src/me/oa10712/bukkitbasiksmagick/BukkitBasiksMagick.java
@@ -10,12 +10,18 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -29,6 +35,7 @@ public class BukkitBasiksMagick extends JavaPlugin {
     private YamlConfiguration magickConfig;
     File dataFolder = new File(server.getWorldContainer().getPath() + File.separator + "plugins" + File.separator + "Bukkit Basiks");
     File magickConfigFile = new File(dataFolder.getPath() + File.separator + "bukkitbasiksconfig.yml");
+    private YamlConfiguration monsterType;
 
     @Override
     public void onEnable() {
@@ -67,12 +74,66 @@ public class BukkitBasiksMagick extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         Player player = null;
-        if(sender instanceof Player){
+        if (sender instanceof Player) {
             player = (Player) sender;
         }
-        if (cmd.getName().equalsIgnoreCase("fireball")) {
-            
+        if (cmd.getName().equalsIgnoreCase("hunger")) {
+            if (player != null) {
+                Player pHurt = server.getPlayer(args[0]);
+                pHurt.setFoodLevel(2);
+                player.sendMessage("Set" + pHurt + "hunger Level to 2");
+            }
+            return true;
         }
+
+        if (cmd.getName().equalsIgnoreCase("summon")) {
+            World w = player.getWorld();
+
+            if (player != null) {
+                EntityType m = EntityType.valueOf(args[0].toUpperCase());
+
+                if (args[0].equals("creeper")) {
+                    w.spawnCreature(player.getTargetBlock(null, 0).getLocation(), m);
+                } else if (args[0].equals("spider")) {
+                    w.spawnCreature(player.getTargetBlock(null, 0).getLocation(), m);
+                } else if (args[0].equals("skeleton")) {
+                    w.spawnCreature(player.getTargetBlock(null, 0).getLocation(), m);
+                } else{
+                    player.sendMessage("Invalid Command");
+                }                   
+            }
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("conjour")) {
+            
+            if (player != null) {
+                ItemStack i = new ItemStack(Material.getMaterial(args[0].toUpperCase()));
+                if(args[0].equals("diamond")){
+                    player.sendMessage("You're chance of diamond = 0%");
+                } else if (args[0].equals("apple")){
+                    player.sendMessage("You're chance of apple = 80%");
+                } else if (args[0].equals("pork")){
+                    player.sendMessage("You're chance of pork = 65%");
+                } else if (args[0].equals("steak")){
+                    player.sendMessage("You're chance of steak = 50%");
+                } else{
+                    player.sendMessage("INVALID ITEM USE /CITEM");
+                }
+            }
+            return true;
+        }
+        
+        if(cmd.getName().equalsIgnoreCase("citem")){
+            
+            if(player != null){
+               player.sendMessage("The items you can conjour are: Diamond, Apple, Pork, Steak");
+            }else{
+                log.info("Must be logged in to use this command.");
+            }
+            return true;
+        }
+
         return false;
     }
 }
